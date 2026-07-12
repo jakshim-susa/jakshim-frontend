616bcf3cbd3ce13e014e9f93176b850e926e0312
jhseo <jhwest.dev@gmail.com>
2026-07-12 17:44:41 +0900

    feat: 이메일 회원가입 기능 구현

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -9,3 +9,12 @@ export const logout = async () => {
     const res = await api.post("/api/auth/logout");
     return res.data;
 };
+
+export const signup = async (data: {
+    email: string;
+    password: string;
+    nickname: string;
+}) => {
+    const res = await api.post("/api/auth/signup", data);
+    return res.data;
+};
